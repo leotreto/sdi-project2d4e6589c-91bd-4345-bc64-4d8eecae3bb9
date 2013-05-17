5b7666d94a3c6a4eb50cb8834d350833331bbe31
leo treto <leo@olga-estradas-imac-10.local>
2013-05-16 21:14:40 -0400

    My first use of Loop, Arrays, Functions

---
+++ Treto_Leonel_assignment_2.js
@@ -0,0 +1,65 @@
+// alert("JavaScript works!");
+// Leonel Treto
+// SDI 1305
+// Week 2 Project Assignment
+// My first activity using Arrays, Loops, Functions, in Javascript
+
+// My variables
+
+var tMinus = 10;
+while (tMinus > 0) {
+// My While Loop countdown
+        console.log(" T-Minus " + tMinus);
+        tMinus--;
+};
+
+console.log(" Ignition, and Lift off. ");
+var months = 18
+
+
+
+    console.log("The shuttle will take " + months + " months to reach the Red Planet. ");
+    
+//My Arrays    
+var ships = ["Alpha", "Omega","Delta"];
+var pilots = ["John", "Luke", "Paul"]; 
+        console.log("Captain " + pilots [1] + " will be piloting shuttle " + ships [1] + ".");
+        
+var speed = true
+if (speed = true){ 
+    console.log("They must maintain a traveling speed of over 30,000 mph to reach Mars in the alotted time. ");
+    
+    } else {  
+    
+    
+    
+    
+    console.log("Shuttle " + ships [1] + " will never make it on time. ");
+    
+    };
+    
+var countUp = function(what,howmany){
+    for (miles = 0; miles <= howmany; miles++){
+        console.log(miles + " hours left to reach Mars " + what + " can't wait to get there. ");
+        console.log("can't wait to get there already. " + what + " are we there yet!! ");
+        
+        if(miles < howmany){
+        
+                console.log("It feels like we are never going to get to Mars. ");
+                console.log("Step on this hunk of junk, " + what + " step on it !!");
+        } else {
+        
+            console.log("I should've staid back on Earth. ");
+        }
+        console.log("");
+    }    
+     
+}
+countUp("Captain Luke ", 10);
+
+
+
+
+
+ 
+			
